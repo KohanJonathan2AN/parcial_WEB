b6bdbd1e2772a0d3950ab31fc492e2b3c592c3bb
Jonathan <Jonathan>
2025-07-14 22:30:37 -0300

    Permisos de conexion con el front

--- src/apps.ts
+++ src/apps.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/user_routes';
 import postRoutes from './routes/post_routes';
 
 const app = express();
-
+app.use(cors({origin: "http://localhost:2575", credentials: true}));
 app.use(express.json());
 
 app.use('/users', userRoutes);
